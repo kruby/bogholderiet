78139c0919615679a2acc59e63923546ce106128
Thomas K. Schmidt <ts@kruby.dk>
2010-01-05 21:53:11 +0100

    En enkelt rettelse vedr. adressen

--- config/deploy.rb
+++ config/deploy.rb
@@ -131,7 +131,7 @@ default_run_options[:pty] = true
 # 'try_sudo' command to run your commands.
 # set :use_sudo, false
 # set :runner, user
-set :mongrel_port, 9300
+set :mongrel_port, 9400
 
 ## Dependencies
 # Set the commands and gems that your application requires. e.g.
