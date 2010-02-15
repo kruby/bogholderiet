1fff1130a7de062d4ee240e483e560dc32763e4b
Thomas K. Schmidt <ts@kruby.dk>
2010-02-15 23:21:37 +0100

    Ved login ender man ved posts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
         current_user.remember_me unless current_user.remember_token?
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
-      redirect_back_or_default('/products')
+      redirect_back_or_default('/posts')
      flash[:notice] = "Du er nu logget ind!"
     else
       flash[:notice] = "Forkert brugernavn eller password!"
